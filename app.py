60b6f73ae621c7b7896b883069a42be38dced041
Swayam Patil <patilswayam96@gmail.com>
2024-09-05 19:00:30 +0530

    Added new project files

---
+++ app.py
@@ -0,0 +1,62 @@
+import pickle
+import streamlit as st
+import requests
+
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url).json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+def recommend(movie):
+    # Check if the movie exists in the DataFrame
+    if movie in movies['title'].values:
+        index = movies[movies['title'] == movie].index[0]
+        distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+        recommended_movie_names = []
+        recommended_movie_posters = []
+        for i in distances[1:6]:
+            # fetch the movie poster
+            movie_id = movies.iloc[i[0]].movie_id
+            recommended_movie_posters.append(fetch_poster(movie_id))
+            recommended_movie_names.append(movies.iloc[i[0]].title)
+        return recommended_movie_names, recommended_movie_posters
+    else:
+        # If movie not found, return empty lists
+        st.error(f"Please Type a movie name in the text box or select a movie from the dropdown.")
+        return [], []
+
+st.title('Movie Recommender System')
+movies = pickle.load(open('model/movie_list.pkl','rb'))
+similarity = pickle.load(open('model/similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Find the perfect movie for any mood with our smart recommendation engine.",
+    movie_list,
+    index=None,
+    placeholder="Type or select a movie from the dropdown",
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    if recommended_movie_names:  # Ensure there are recommendations before displaying
+        col1, col2, col3, col4, col5 = st.columns(5, gap="small")
+        with col1:
+            st.text(recommended_movie_names[0])
+            st.image(recommended_movie_posters[0])
+        with col2:
+            st.text(recommended_movie_names[1])
+            st.image(recommended_movie_posters[1])
+        with col3:
+            st.text(recommended_movie_names[2])
+            st.image(recommended_movie_posters[2])
+        with col4:
+            st.text(recommended_movie_names[3])
+            st.image(recommended_movie_posters[3])
+        with col5:
+            st.text(recommended_movie_names[4])
+            st.image(recommended_movie_posters[4])
+
+
